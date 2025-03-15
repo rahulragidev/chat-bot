cd118bfdc9f98af28457367899224d7de240fdb1
Jonathan Wilke <info@jonathan-wilke.de>
2025-03-15 13:37:14 +0100

    fix: rounded corners

--- apps/web/modules/ui/components/card.tsx
+++ apps/web/modules/ui/components/card.tsx
@@ -8,7 +8,7 @@ const Card = React.forwardRef<
 	<div
 		ref={ref}
 		className={cn(
-			"rounded-3xl bg-card text-card-foreground border",
+			"rounded-2xl bg-card text-card-foreground border",
 			className,
 		)}
 		{...props}
