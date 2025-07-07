64c8ca17759a07f82821c454098c17fd7c3fcbc1
Jonathan <info@jonathan-wilke.de>
2025-07-07 21:58:48 +0200

    chore: try prisma monorepo workaround plugin

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,4 +1,6 @@
 import { withContentCollections } from "@content-collections/next";
+// @ts-expect-error - PrismaPlugin is not typed
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 import nextIntlPlugin from "next-intl/plugin";
 
@@ -42,12 +44,16 @@ const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-	webpack: (config, { webpack }) => {
+	webpack: (config, { webpack, isServer }) => {
 		config.plugins.push(
 			new webpack.IgnorePlugin({
 				resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
 			}),
 		);
+
+		if (isServer) {
+			config.plugins.push(new PrismaPlugin());
+		}
 
 		return config;
 	},
